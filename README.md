0c40ff1e21c15bf8eb72e07f423d99bdf0d07af5
Van Truong <43657607+vtruo009@users.noreply.github.com>
2023-05-29 15:45:09 -0700

    Create README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notion Financial Tracker
+This project is used to solve the problem of having to manually track spendings and budget. Existing application seems to be too complicated for my use.
--- src/config/integration.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NOTION_DATABASE_ID = exports.NOTION_SECRET = void 0;
-exports.NOTION_SECRET = process.env['NOTION_SECRET'] || '';
-exports.NOTION_DATABASE_ID = process.env['NOTION_DATABASE_ID'] || '';
