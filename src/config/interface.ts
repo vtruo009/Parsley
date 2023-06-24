e958ba6a9545930601822ae05504e7d80e5cdf19
Van Truong <43657607+vtruo009@users.noreply.github.com>
2023-06-24 04:06:45 +0000

    initial refactoring

--- src/config/interface.ts
+++ src/config/interface.ts
@@ -1,22 +1,21 @@
 /**
  * Defines the properties to be specified in instances of {@link TransactionItem}
  */
-export interface TransactionItem {
-    transactionDate: string;
-    postedDate: string;
-    description: string;
-    amount: number;
-    category: TransactionCategory;
-    type: 'sale';
-    memo: string;
+export enum Category {
+    BILLS = 'Bills & Utilities',
+    GROCERIES = 'Groceries',
+    SHOPPING = 'Shopping',
+    FOOD_AND_DRINKS = 'Food & Drinks',
 }
 
 /**
  * The category the transaction belongs to
  */
-export enum TransactionCategory {
-    BILLS = 'Bills & utilities',
-    GROCERY = 'Grocery',
-    SHOPPING = 'Shopping',
-    FOOD_AND_DRINKS = 'Food & drinks',
+export interface transactionItem {
+    transactionDate: string;
+    postedDate: string;
+    description: string;
+    amount: number;
+    category: Category;
+    memo: string;
 }
