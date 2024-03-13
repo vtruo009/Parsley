de00dab1d9c616acb9b3ffe4b8108ee131db8f76
Van Truong <van.t414@gmail.com>
2024-03-13 15:47:47 -0700

    setup package.json

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,22 @@ import { getDatabase } from './notion-api/database';
 export const notion = new Client({ auth: NOTION_SECRET });
 
 function main() {
-    // const transactions = createTransactionData();
-    // for (let transaction of transactions) {
-    //     createPage(transaction);
-    // }
+    const transactions = createTransactions();
+    for (let transaction of transactions) {
+        createPage(transaction);
+    }
     // getDatabase().then(res => {
     //     console.log(res)
     // }).catch(err => {
     //     console.log(err)
     // })
-    getPage('e4c40637eb724649a28b9881b15199ca')
-        .then(res => {
-            console.log(res)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+    // getPage('e4c40637eb724649a28b9881b15199ca')
+    //     .then(res => {
+    //         console.log(res)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
 }
 
 main();
