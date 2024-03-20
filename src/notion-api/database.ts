992b003ba858b9579a64ed3cc7bfa8625e32ad60
Van Truong <van.t414@gmail.com>
2024-03-20 12:59:36 -0700

    remove unused files

--- src/notion-api/database.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import { notion } from "..";
-
-dotenv.config()
-
-export async function getDatabase() {
-    return await notion.databases.retrieve({ database_id: process.env.NOTION_DATABASE_ID || '' });
-}
