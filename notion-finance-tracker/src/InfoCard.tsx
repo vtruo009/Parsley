ece5db5f897e2dec7cc00a0d7bb6e82c86aa6421
Van Truong <van.t414@gmail.com>
2024-03-23 22:23:13 -0700

    add shadow effect

--- notion-finance-tracker/src/InfoCard.tsx
+++ notion-finance-tracker/src/InfoCard.tsx
@@ -6,6 +6,14 @@ const StyledDiv = styled.div`
     height: 600px;
     border: solid 2px gray;
     border-radius: 20px;
+    background-color: #F5F5F2;
+    box-shadow:
+      0 1px 1px hsl(0deg 0% 0% / 0.075),
+      0 2px 2px hsl(0deg 0% 0% / 0.075),
+      0 4px 4px hsl(0deg 0% 0% / 0.075),
+      0 8px 8px hsl(0deg 0% 0% / 0.075),
+      0 16px 16px hsl(0deg 0% 0% / 0.075)
+    ;
 `;
 
 const StyledTitle = styled.h3`
